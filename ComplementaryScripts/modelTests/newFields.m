9b2a8e20638c7bd2062dbd158b0cf4098ec03faf
Benjamin Sanchez <bjsanchezb@gmail.com>
2020-09-01 16:58:34 +0200

    test: keeping new fields

---
+++ ComplementaryScripts/modelTests/newFields.m
@@ -0,0 +1,23 @@
+% metEnviPathID
+model.metEnviPathID = cell(size(model.mets));
+model.metEnviPathID{1} = '32de3cf4-e3e6-4168-956e-32fa5ddb0ce1/compound/b545cabc-8c9e-4b20-8848-efa015b481ea';
+
+% metLIPIDMAPSID
+model.metLIPIDMAPSID = cell(size(model.mets));
+model.metLIPIDMAPSID{1} = 'LMPR0102010012';
+
+% metSABIORKID
+model.metSABIORKID = cell(size(model.mets));
+model.metSABIORKID{1} = '123';
+
+% metSLMID
+model.metSLMID = cell(size(model.mets));
+model.metSLMID{1} = 'SLM:000048885';
+
+% rxnKEGGPathways
+model.rxnKEGGPathways = cell(size(model.rxns));
+model.rxnKEGGPathways{1} = 'hsa00620';
+
+% rxnSABIORKID
+model.rxnSABIORKID = cell(size(model.rxns));
+model.rxnSABIORKID{1} = '123';
